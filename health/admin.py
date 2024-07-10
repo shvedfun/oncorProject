25488449a1f468b71875af63b9479e545f2f8ae1
VicS <shvedfun@gmail.com>
2024-07-10 16:25:58 +0300

    add to admin panel

--- health/admin.py
+++ health/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from health.models import Region, Person
+from health.models import Region, Person, DiseaseCategory, Disease, Direction, StageDisease, PersonDisease
 
 # Register your models here.
 @admin.register(Region)
@@ -13,3 +13,25 @@ class RegionAdmin(admin.ModelAdmin):
 class PersonAdmin(admin.ModelAdmin):
     pass
     list_filter = ('region__code',)
+
+
+@admin.register(DiseaseCategory)
+class DiseaseCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(StageDisease)
+class StageDiseaseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PersonDisease)
+class PersonDiseaseAdmin(admin.ModelAdmin):
+    pass
